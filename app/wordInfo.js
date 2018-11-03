dcd637959c4ef14d5fab83d64f513a5bb3f79855
Mahendra-Singh-Chauhan <m.chauhankkm@gmail.com>
2018-11-03 14:52:22 +0530

    update-and-all-dict-for-word

--- app/wordInfo.js
+++ app/wordInfo.js
@@ -1,163 +1,183 @@
-let constants= require('./constants')
+let constants = require('./constants')
 var request = require('request');
-let getDefinition = (word)=>{
+let getDefinition = (word) => {
     var options = {
         url: `${constants.baseUrl}/entries/en/${word}`,
         headers: {
             'app_id': constants.apiId,
-            'app_key':constants.apiKey
+            'app_key': constants.apiKey
         }
     };
     // Return new promise 
-    return new Promise(function(resolve, reject) {
-     // Do async job
-        request.get(options, function(err, resp, body) {
+    return new Promise(function (resolve, reject) {
+        // Do async job
+        request.get(options, function (err, resp, body) {
             if (err) {
                 reject(err);
             } else {
-                let obj = JSON.parse(body)
-              
-              //  console.log(obj.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0])
-              let definition = obj.results[0].lexicalEntries[0].entries[0].senses;
-              resolve(definition)
+               // console.log(resp.statusCode)
+                if(resp.statusCode==404){
+                    console.log("Oops! Word does not exist")
+                }else{
+                   let obj = JSON.parse(body)
+
+                //  console.log(obj.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0])
+                let definition = obj.results[0].lexicalEntries[0].entries[0].senses;
+                let arr = []
+                for (let i in definition) {
+                    arr.push(definition[i].definitions)
+                }
+               resolve(arr)
+                }
+               
 
             }
         })
     })
 }
 
-let getSynonyms = (word)=>{
+let getSynonyms = (word) => {
     var options = {
         url: `${constants.baseUrl}/entries/en/${word}/synonyms`,
         headers: {
             'app_id': constants.apiId,
-            'app_key':constants.apiKey
+            'app_key': constants.apiKey
         }
     };
     // Return new promise 
-    return new Promise(function(resolve, reject) {
-     // Do async job
-        request.get(options, function(err, resp, body) {
+    return new Promise(function (resolve, reject) {
+        // Do async job
+        request.get(options, function (err, resp, body) {
             if (err) {
                 reject(err);
             } else {
+
+                if(resp.statusCode==404){
+                    console.log("Oops! Word does not exist")
+                }else{
                 let obj = JSON.parse(body)
-              
-            // console.log(obj)
-             let synonyms =obj.results[0].lexicalEntries[0].entries[0].senses[0].synonyms
-          resolve(synonyms)
+
+                // console.log(obj)
+                let synonyms = obj.results[0].lexicalEntries[0].entries[0].senses[0].synonyms
+                let arr = []
+                for (let i in synonyms) {
+                    arr.push(synonyms[i].text)
+                }
+                resolve(arr)
+            }
 
             }
         })
     })
 }
 
-let getAntonyms = (word)=>{
+let getAntonyms = (word) => {
     var options = {
         url: `${constants.baseUrl}/entries/en/${word}/antonyms`,
         headers: {
             'app_id': constants.apiId,
-            'app_key':constants.apiKey
+            'app_key': constants.apiKey
         }
     };
     // Return new promise 
-    return new Promise(function(resolve, reject) {
-     // Do async job
-        request.get(options, function(err, resp, body) {
+    return new Promise(function (resolve, reject) {
+        // Do async job
+        request.get(options, function (err, resp, body) {
             if (err) {
                 reject(err);
             } else {
-                let obj = JSON.parse(body)
-              
-              //  console.log(obj.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0])
-              let definition = obj.results[0].lexicalEntries[0].entries[0].senses[0].antonyms;
-              resolve(definition)
 
+                if(resp.statusCode==404){
+                    console.log("Oops! Word does not exist")
+                }else{
+                let obj = JSON.parse(body)
+
+                //  console.log(obj.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0])
+                let antonyms = obj.results[0].lexicalEntries[0].entries[0].senses[0].antonyms;
+                let arr = []
+                for (let i in antonyms) {
+                    arr.push(antonyms[i].text)
+                }
+                resolve(arr)
+            }
             }
         })
     })
 }
 
-let getExamples = (word)=>{
+let getExamples = (word) => {
     var options = {
         url: `${constants.baseUrl}/entries/en/${word}`,
         headers: {
             'app_id': constants.apiId,
-            'app_key':constants.apiKey
+            'app_key': constants.apiKey
         }
     };
     // Return new promise 
-    return new Promise(function(resolve, reject) {
-     // Do async job
-        request.get(options, function(err, resp, body) {
+    return new Promise(function (resolve, reject) {
+        // Do async job
+        request.get(options, function (err, resp, body) {
             if (err) {
                 reject(err);
             } else {
+                if(resp.statusCode==404){
+                    console.log("Oops! Word does not exist")
+                }else{
                 let obj = JSON.parse(body)
-              
-              //  console.log(obj.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0])
-              let examples = obj.results[0].lexicalEntries[0].entries[0].senses;
-              resolve(examples)
 
+                //  console.log(obj.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0])
+                let examples = obj.results[0].lexicalEntries[0].entries[0].senses;
+                resolve(examples)
+                }
             }
         })
     })
 }
 
-let getAllInfoOfWord = (word)=>{
-    var options = {
-        url: `${constants.baseUrl}/entries/en/${word}`,
-        headers: {
-            'app_id': constants.apiId,
-            'app_key':constants.apiKey
-        }
-    };
-    // Return new promise 
-    return new Promise(function(resolve, reject) {
-     // Do async job
-        request.get(options, function(err, resp, body) {
-            if (err) {
-                reject(err);
-            } else {
-                let obj = JSON.parse(body)
-              
-              //  console.log(obj.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0])
-              let definition = obj.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0];
-              resolve(definition)
+let getAllInfoOfWord = (word) => {
 
-            }
-        })
-    })
+    let definition= getDefinition(word)
+    let synonyms=getSynonyms(word)
+    let antonyms=  getAntonyms(word)
+ 
+ return  Promise.all([definition,synonyms, antonyms])
+
+    
 }
 
-let getInfoOfWordOfDay = (word)=>{
+let getInfoOfWordOfDay = (word) => {
     var options = {
         url: `${constants.baseUrl}/entries/en/${word}`,
         headers: {
             'app_id': constants.apiId,
-            'app_key':constants.apiKey
+            'app_key': constants.apiKey
         }
     };
     // Return new promise 
-    return new Promise(function(resolve, reject) {
-     // Do async job
-        request.get(options, function(err, resp, body) {
+    return new Promise(function (resolve, reject) {
+        // Do async job
+        request.get(options, function (err, resp, body) {
             if (err) {
                 reject(err);
             } else {
-                let obj = JSON.parse(body)
-              
-              //  console.log(obj.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0])
-              let definition = obj.results[0].lexicalEntries[0].entries[0].senses;
-              resolve(definition)
-
+                if(resp.statusCode==404){
+                    console.log("Oops! Word does not exist")
+                }else{
+                console.log(body)
+                // let obj = JSON.parse(body)
+               
+                //  console.log(obj.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0])
+                // let definition = obj.results[0].lexicalEntries[0].entries[0].senses;
+                // resolve(definition)
+                }
             }
         })
     })
 }
 module.exports = {
-    getDefinition:getDefinition,
-    getExamples:getExamples,
-    getSynonyms:getSynonyms,
-    getAntonyms:getAntonyms
+    getDefinition: getDefinition,
+    getExamples: getExamples,
+    getSynonyms: getSynonyms,
+    getAntonyms: getAntonyms,
+    getAllInfoOfWord:getAllInfoOfWord
 }
--- index.js
+++ index.js
@@ -6,87 +6,175 @@ let dictionary = {
 };
 
 
-let readline = require('readline');
-let rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false
-});
+// let readline = require('readline');
+// let rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+//   terminal: false
+// });
 
-rl.on('line', (input) => {
+let executeCommands = (cmd, word) => {
+  console.log(cmd);
+  console.log(word)
+  if (cmd == undefined) {
+    console.log("all details of word of the day")
+  } else {
 
-  let finalInput = input.toString().trim().split(" ");
-  console.log(finalInput)
-  if (finalInput[0] == dictionary.firstArgument) {
-    if (dictionary.gameMode) {
-
-    } else {
-      if (finalInput[1] == constants.types.definition) {
-        wordInfo.getDefinition(finalInput[2]).then((resolve) => {
-          console.log(`"Definitions : "`)
-          //  console.log(resolve)
-          for (let i in resolve) {
-            console.log(`${Number(i) + 1}. ${resolve[i].definitions}`)
-          }
-        }).catch((err) => {
-          console.log(err)
-        })
-      } else if (finalInput[1] == constants.types.synonyms) {
-        wordInfo.getSynonyms(finalInput[2]).then((resolve) => {
-          console.log(`"Synonyms : "`)
-          //  console.log(resolve)
-         let x = 1;
-          for (let i in resolve) {
-              console.log(`${x}. ${resolve[i].text}`)
-              x++;
-          }
-        }).catch((err) => {
-          console.log(err)
-        })
-      } else if (finalInput[1] == constants.types.antonyms) {
-        wordInfo.getAntonyms(finalInput[2]).then((resolve) => {
-          console.log(`"Antonyms : "`)
-          //  console.log(resolve)
-         let x = 1;
-          for (let i in resolve) {
-              console.log(`${x}. ${resolve[i].text}`)
-              x++;
-          }
-        }).catch((err) => {
-          console.log(err)
-        })
-      } else if (finalInput[1] == constants.types.examples) {
-        wordInfo.getExamples(finalInput[2]).then((resolve) => {
-          console.log(`"Examples : "`)
-         
-          let x = 1;
-          for (let i in resolve) {
-            let arr = resolve[i].examples;
-            for (let j in arr) {
-              console.log(`${x}. ${arr[j].text}`)
+    switch (cmd) {
+      case 'def':
+        if (word == undefined) {
+          console.log("Please enter word");
+        } else {
+          wordInfo.getDefinition(word).then((resolve) => {
+            console.log(`"Definitions : "`)
+              console.log(resolve)
+            let x = 1;
+            for (let i in resolve) {
+              console.log(`${x}. ${resolve[i]}`)
               x++;
             }
 
-          }
-        }).catch((err) => {
-          console.log(err)
-        })
-      } else if (finalInput[1] == constants.types.playGame) {
-        console.log("start word game")
-      } else if (finalInput[1] == constants.types.fullDict) {
-        console.log("full dict of word")
-      } else if (finalInput[1] == undefined) {
-        console.log("all details of word of the day")
-      } else {
-        console.log("full dict of word")
+          }).catch((err) => {
+           
+            console.log(err)
+          })
+        }
+
+        break;
+      case 'syn':
+        if (word == undefined) {
+          console.log("Please enter word");
+        } else {
+          wordInfo.getSynonyms(word).then((resolve) => {
+            console.log(`"Synonyms : "`)
+            //  console.log(resolve)
+            let x = 1;
+            for (let i in resolve) {
+              console.log(`${x}. ${resolve[i]}`)
+              x++;
+            }
+          }).catch((err) => {
+            console.log(err)
+          })
+        }
+
+
+        break;
+      case 'ant':
+        if (word == undefined) {
+          console.log("Please enter word");
+        } else {
+          wordInfo.getAntonyms(word).then((resolve) => {
+            console.log(`"Antonyms : "`)
+            //  console.log(resolve)
+            let x = 1;
+            for (let i in resolve) {
+              console.log(`${x}. ${resolve[i]}`)
+              x++;
+            }
+          }).catch((err) => {
+            console.log(err)
+          })
+        }
+
+        break;
+
+      case 'ex':
+        if (word == undefined) {
+          console.log("Please enter word");
+        } else {
+          wordInfo.getExamples(word).then((resolve) => {
+            console.log(`"Examples : "`)
+
+            let x = 1;
+            for (let i in resolve) {
+              let arr = resolve[i].examples;
+              for (let j in arr) {
+                console.log(`${x}. ${arr[j].text}`)
+                x++;
+              }
+
+            }
+          }).catch((err) => {
+            console.log(err)
+          })
+        }
+
+        break;
+
+      case 'play':
+        console.log("start word game");
+        break;
+      case 'dict':
+        if (word == undefined) {
+          console.log("Please enter word");
+        } else {
+          wordInfo.getAllInfoOfWord(word).then((resolve) => {
+            printWordInfo(resolve)
+          })
+        }
+
+        break;
+      default:
+        if (word != undefined) {
+          console.log("please enter correct command")
+        }else{
+          wordInfo.getAllInfoOfWord(cmd).then((resolve) => {
+            printWordInfo(resolve)
+          })
+        }
+       
+    }
+
+  }
+
+
+}
+
+// rl.on('line', (input) => {
+
+//   let finalInput = input.toString().trim().split(" ");
+//   console.log(finalInput)
+//   if (finalInput[0] == dictionary.firstArgument) {
+//     if (dictionary.gameMode) {
+
+//     } else {
+
+//     }
+//   } else {
+//     console.log("Invalid input")
+//   }
+// })
+
+let printWordInfo = (resolve) => {
+  for (let x in resolve) {
+    if (x == 0) {
+      console.log("'Definitions':")
+      let y = 1;
+      for (let j of resolve[x]) {
+        console.log(`${y}. ${j}`);
+        y++;
+      }
+    } else if (x == 1) {
+      console.log("'Synonyms':")
+      let y = 1;
+      for (let j of resolve[x]) {
+        console.log(`${y}. ${j}`);
+        y++;
+      }
+    } else if (x == 2) {
+      console.log("'Antonyms':")
+      let y = 1;
+      for (let j of resolve[x]) {
+        console.log(`${y}. ${j}`);
+        y++;
       }
     }
-  } else {
-    console.log("Invalid input")
+    console.log()
   }
-})
-
-
+}
 console.log("Welcome to Command Line Dictionary");
-console.log("Enter commands as mentioned");
 
+module.exports = {
+  executeCommands: executeCommands
+}
